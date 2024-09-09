9be6659a60c5132fc456a81d870ddb38f121fd80
Sufilyas <sufinalyas@gmail.com>
2024-09-09 17:47:44 +0800

    Update app.py
    add plotly scatter graph

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+import numpy as np 
+import pandas as pd 
+import plotly.express as px
+
+#H1 is the header for writing
+#app.title give the file a name otherwise they name defaultly
 
 app = Dash(__name__)
+app.title = "Exercise 3"
 server = app.server
 
-#H1 is header for writing
+df = pd.read_csv("https://github.com/Sufilyas/MCM7183Exercise3/blob/main/assets/gdp_1960_2020.csv")
+subset_my = df[df['country'].isin(["Malaysia"])]
+fig = px.scatter(subset_my, x = "year", y = "gdp")
 
 app.layout = html.H1('Trulululu')
 
