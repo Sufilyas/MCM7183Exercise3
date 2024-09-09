d96115dc9621d2396f64f81f2f32a058a8bb6372
Sufilyas <sufinalyas@gmail.com>
2024-09-09 19:44:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ image_path = 'assets/Tired_Happy.png'
 
 app.layout = [html.H1('Trulululu'), 
               html.Img(src=image_path),
-              dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='dropdown-country'), 
+              dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 
+                           'Malaysia', id='dropdown-country'), 
               dcc.Graph(id="grahp-scatter"), 
               dcc.Dropdown([{'label':'2020', 'value': 2020}, 
                             {'label':'2010', 'value': 2010}, 
@@ -43,7 +44,11 @@ def update_graph(country_selected, year_selected):
     subset_year_America = subset_year[subset_year['state'].isin(["America"])]
     subset_year_Europe = subset_year[subset_year['state'].isin(["Europe"])]
     subset_year_Oceania = subset_year[subset_year['state'].isin(["Oceania"])]
-    pie_data = [sum(subset_year_Asia['gdp']),sum(subset_year_Africa['gdp']),sum(subset_year_America['gdp']),sum(subset_year_Europe['gdp']),sum(subset_year_Oceania['gdp'])];
+    pie_data = [sum(subset_year_Asia['gdp']),
+                sum(subset_year_Africa['gdp']),
+                sum(subset_year_America['gdp']),
+                sum(subset_year_Europe['gdp']),
+                sum(subset_year_Oceania['gdp'])];
     mylabels = ["Asia","Africa","America","Europe","Oceania"]
     pie_df = {'continent': mylabels,'gdp': pie_data}
     fig2 = px.pie(pie_df,values = "gdp", names = "continent")
