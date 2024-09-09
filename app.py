89c75128859d8eed60ec5c5fc3fa0c9d534fdc8b
Sufilyas <sufinalyas@gmail.com>
2024-09-09 17:57:41 +0800

    Update app.py
    add piechart

--- app.py
+++ app.py
@@ -12,7 +12,19 @@ server = app.server
 
 df = pd.read_csv("https://github.com/Sufilyas/MCM7183Exercise3/blob/main/assets/gdp_1960_2020.csv")
 subset_my = df[df['country'].isin(["Malaysia"])]
-fig = px.scatter(subset_my, x = "year", y = "gdp")
+fig = px.scatter(subset_my, x = "year", y = "GDP")
+
+subset_2020_Asia = subset_2020[subset_2020['state'].isin(["Asia"])]
+subset_2020_Africa = subset_2020[subset_2020['state'].isin(["Africa"])]
+subset_2020_America = subset_2020[subset_2020['state'].isin(["America"])]
+subset_2020_Europe = subset_2020[subset_2020['state'].isin(["Europe"])]
+subset_2020_Oceania = subset_2020[subset_2020['state'].isin(["Oceania"])]
+pie_data = [sum(subset_2020_Asia['gdp']),sum(subset_2020_Africa['gdp']),sum(subset_2020_America['gdp']),sum(subset_2020_Europe['gdp']),sum(subset_2020_Oceania['gdp'])];
+mylabels = ["Asia","Africa","America","Europe","Oceania"]
+plt.pie(pie_data, labels = mylabels)
+pie_df = {'continent': mylabels,'gdp': pie_data}
+fig2 = px.pie(pie_df,values = "gdp", names = "continent")
+
 
 app.layout = html.H1('Trulululu')
 
